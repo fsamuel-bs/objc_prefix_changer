c672842f4d9fac824ebf6f2ab883664fab2742bd
Samuel Barroso <fsamuel.bs@gmail.com>
2014-11-02 18:07:10 -0500

    [V2] Add 'multithreading'
    - since the runtime seemed to get longer, use bash's multithreading
      to accelerate implementation;
    - create background processes, storing their pids in a 'stack'. Then,
      just wait for them to finish
    
    Better project architecture, creating functions for specific tasks

--- prefix_changer.sh
+++ prefix_changer.sh
@@ -1,93 +1,110 @@
 #!/bin/bash
+
+#CLOSE XCODE BEFORE RUNNING THE SCRIPT. IT ALSO HAS GIT PRIVELEGES AND MESSES UP 
+# GIT MV EXECUTED IN THIS PROJECT
+
 set -e 
 
-replace_filename_in_file () {
-    sed  -i '' -l "s/\([\.\"\* ]\)$1\([\.\"\* ]\)/\1$2\2/g" $3
-}
-export -f replace_filename_in_file
-
-
-all_postfix=".m .h .xib"
+all_extensions=".m .h .xib"
 prefix_from=`# PUT OLD PREFIX HERE`
 prefix_to=`# PUT NEW PREFIX HERE`
 project_name=`# PUT PROJECT NAME HERE`
 
-for postfix in $all_postfix
-do
-  files=`find $project_name -type f -name "*$postfix" | grep -oh "\/\w*$postfix*$" | sed "s/^\/\([a-zA-Z]*\)$postfix$/\1/g"`
+#replace_filename_in_file $file_name $new_file_name $file_to_replace_pattern
+replace_filename_in_file () {
+  sed  -i '' -l "s/\([^a-zA-Z]\)$1\([^a-zA-Z]\)/\1$2\2/g" $3 &&
+    sed  -i '' -l "s/^$1\([^a-zA-Z]\)/$2\1/g" $3 &&
+    sed  -i '' -l "s/\([^a-zA-Z]\)$1$/\1$2/g" $3 &&
+    sed  -i '' -l "s/^$1$/$2/g" $3
+}
+export -f replace_filename_in_file
 
-  for file_name in $files
-  do
-    if [[ "$file_name" == *"$prefix_from"* ]]; then
-      file_name_without_prefix="${file_name:${#prefix_from}}"
-      new_file_name="$prefix_to$file_name_without_prefix"
-    else
-      new_file_name="$prefix_to$file_name"
-    fi
+#replace_filename_in_all_files $file_name $new_file_nam
+replace_filename_in_all_files() {
+  files_with_m=`find $project_name -type f -name "*.m"`
+  files_with_h=`find $project_name -type f -name "*.h"`
+  files_with_xib=`find $project_name -type f -name "*.xib"`
+  project=`find ${project_name}.xcodeproj -name "project.pbxproj"`
+  storyboard=`find . -name "MainStoryboard.storyboard"`
 
-    echo "Current file name: $file_name"
-    echo "New file name: $new_file_name"
+  stack=()
 
-    all_old_paths=`find $project_name -type f -name "$file_name*$postfix"`
-    for old_path in $all_old_paths
-    do
-      new_path=`echo $old_path | sed "s/$file_name/$new_file_name/g"`
-      echo "Old path: $old_path"
-      echo "New path: $new_path"
-      git mv $old_path $new_path
-    done
+  ( for file_m in $files_with_m; do
+    replace_filename_in_file $1 $2 $file_m
+  done )& stack[0]=$!
 
-    echo '---------------------------------------------'
+  ( for file_h in $files_with_h; do
+    replace_filename_in_file $1 $2 $file_h
+  done )& stack[1]=$!
 
-    stack=()
-    echo "Rename file reference with name ending in *$postfix"
-    files_with_m=`find $project_name -type f -name "*.m"`
-    files_with_h=`find $project_name -type f -name "*.h"`
-    files_with_xib=`find $project_name -type f -name "*.xib"`
-    project=`find . -name "project.pbxproj"`
-    storyboard=`find . -name "MainStoryboard.storyboard"`
-    ( for file_m in $files_with_m; do
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name\([^a-zA-Z]\)/\1$new_file_name\2/g" $file_m &&
-        sed  -i '' -l "s/^$file_name\([^a-zA-Z]\)/$new_file_name\1/g" $file_m &&
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name$/\1$new_file_name/g" $file_m &&
-        sed  -i '' -l "s/^$file_name$/$new_file_name/g" $file_m
-      done )& stack[0]=$!
-    
-    ( for file_h in $files_with_h; do
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name\([^a-zA-Z]\)/\1$new_file_name\2/g" $file_h &&
-        sed  -i '' -l "s/^$file_name\([^a-zA-Z]\)/$new_file_name\1/g" $file_h &&
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name$/\1$new_file_name/g" $file_h &&
-        sed  -i '' -l "s/^$file_name$/$new_file_name/g" $file_h
-      done )& stack[1]=$!
+  ( for file_xib in $files_with_xib; do
+    replace_filename_in_file $1 $2 $file_xib
+  done )& stack[2]=$!
 
-    ( for file_xib in $files_with_xib; do
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name\([^a-zA-Z]\)/\1$new_file_name\2/g" $file_xib &&
-        sed  -i '' -l "s/^$file_name\([^a-zA-Z]\)/$new_file_name\1/g" $file_xib &&
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name$/\1$new_file_name/g" $file_xib &&
-        sed  -i '' -l "s/^$file_name$/$new_file_name/g" $file_xib
-      done )& stack[2]=$!
-    
-    (   sed  -i '' -l "s/\([^a-zA-Z]\)$file_name\([^a-zA-Z]\)/\1$new_file_name\2/g" $project &&
-        sed  -i '' -l "s/^$file_name\([^a-zA-Z]\)/$new_file_name\1/g" $project &&
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name$/\1$new_file_name/g" $project &&
-        sed  -i '' -l "s/^$file_name$/$new_file_name/g" $project
-    )& stack[3]=$!
-   
-    (   sed  -i '' -l "s/\([^a-zA-Z]\)$file_name\([^a-zA-Z]\)/\1$new_file_name\2/g" $storyboard &&
-        sed  -i '' -l "s/^$file_name\([^a-zA-Z]\)/$new_file_name\1/g" $storyboard &&
-        sed  -i '' -l "s/\([^a-zA-Z]\)$file_name$/\1$new_file_name/g" $storyboard &&
-        sed  -i '' -l "s/^$file_name$/$new_file_name/g" $storyboard
-    )& stack[4]=$!
-    
-    for job in ${stack[@]}; do
-      wait $job
-    done
-#    find . -name "project.pbxproj" -exec sed  -i '' -l "s/\([\.\"\* ^a-z^A-Z]\)$file_name\([\.\"\* ]*\)/\1$new_file_name\2/g" {} ";" 
-#    
-#    if $file_name && $new_word; then
-#      #Executed ${#all_postfix} times. REFACTOR!
-#      find $project_name -type f -exec sed  -i '' -l "s/\([\.\"\* ]\)$file_name\([\.\"\* ]\)/\1$new_word\2/g" {} ";"
-#      find $project_name.xcodeproj -type f -exec sed  -i '' -l "s/\([\.\"\* ]\)$file_name\([\.\"\* ]\)/\1$new_word\2/g" {} ";"
-#    fi
+  ( replace_filename_in_file $1 $2 $project
+  )& stack[3]=$!
+
+  ( replace_filename_in_file $1 $2 $storyboard
+  )& stack[4]=$!
+
+  for job in ${stack[@]}; do
+    wait $job
   done
-done
+}
+export -f replace_filename_in_all_files
+
+#replace_path $file_name $new_file_name $extension
+replace_path () {
+  all_old_paths=`find $project_name -type f -name "$1*$3"`
+  for old_path in $all_old_paths
+  do
+    new_path=`echo $old_path | sed "s/$1/$2/g"`
+    echo "Old path: $old_path"
+    echo "New path: $new_path"
+    git mv $old_path $new_path
+  done
+}
+export -f replace_path
+
+
+main () {
+  for extension in $all_extensions
+  do
+    files=`find $project_name -type f -name "*$extension" | grep -o "\/\w*$extension*$" |
+    sed "s/^\/\([a-zA-Z]*\)$extension$/\1/g"`
+
+    for file_name in $files
+    do
+      if [[ "$file_name" == *"$prefix_from"* ]]; then
+        file_name_without_prefix="${file_name:${#prefix_from}}"
+        new_file_name="$prefix_to$file_name_without_prefix"
+      else
+        new_file_name="$prefix_to$file_name"
+      fi
+
+      echo "Current file name: $file_name"
+      echo "New file name: $new_file_name"
+
+      replace_path $file_name $new_file_name $extension
+
+      echo '---------------------------------------------'
+
+      echo "Rename file reference with name ending in *$extension"
+
+
+      #Just execute when not in stash (still implementing)
+      replace_filename_in_all_files $file_name $new_file_name
+    done
+  done
+
+  #Corrects possible changes in main.m
+  replace_path "${prefix_to}main" "main" ".m"
+  replace_filename_in_all_files "${prefix_to}main" "main"
+
+  #Sets standard header to new one
+  find ${project_name}.xcodeproj -name "project.pbxproj" \
+    -exec sed -i "" -l "s/\(CLASSPREFIX = \)${prefix_from}/\1${prefix_to}/g" {} ";"
+}
+
+main
+git add .
